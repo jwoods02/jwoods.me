454ec9bc43731d7b63d36321b61b789169001b7f
James Woods <jwwoods01@gmail.com>
2014-12-03 14:31:51 +0000

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,13 @@
 // Smooth Scroll
 $('a[href*=#]:not([href=#])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
         || location.hostname == this.hostname) {
 
         var target = $(this.hash);
         target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-        if (target.length) {
-            $('html,body').animate({
-                scrollTop: target.offset().top
+           if (target.length) {
+             $('html,body').animate({
+                 scrollTop: target.offset().top
             }, 1000);
             return false;
         }
