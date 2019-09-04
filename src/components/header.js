a43da482025e9e23d1027eeb4abb90b146cda67a
James Woods <jamesw@giffgaff.co.uk>
2019-09-04 11:17:09 +0100

    #28 - Added header component with Material UI

--- src/components/header.js
+++ src/components/header.js
@@ -1,35 +1,64 @@
-import { Link } from "gatsby"
+// import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import { loadCSS } from "fg-loadcss"
+import { makeStyles } from "@material-ui/core/styles"
+import AppBar from "@material-ui/core/AppBar"
+import Toolbar from "@material-ui/core/Toolbar"
+import Typography from "@material-ui/core/Typography"
+import Button from "@material-ui/core/Button"
+import IconButton from "@material-ui/core/IconButton"
+import Icon from "@material-ui/core/Icon"
+import { Container } from "@material-ui/core"
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
-)
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+    paddingLeft: 0,
+    paddingRight: 0,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+  icon: {
+    color: "#FFFFFF",
+  },
+}))
+
+function Header({ siteTitle }) {
+  const classes = useStyles()
+
+  React.useEffect(() => {
+    loadCSS(
+      "https://use.fontawesome.com/releases/v5.1.0/css/all.css",
+      document.querySelector("#font-awesome-css")
+    )
+  }, [])
+
+  return (
+    <AppBar color="primary">
+      <Container maxWidth="lg">
+        <Toolbar className={classes.root}>
+          <Typography variant="h6" className={classes.title}>
+            James Woods
+          </Typography>
+          <Button color="inherit">Work</Button>
+          <Button color="inherit">Blog</Button>
+          <Button color="inherit">About</Button>
+          <IconButton>
+            <Icon className={"fab fa-github " + classes.icon}></Icon>
+          </IconButton>
+          <IconButton>
+            <Icon className={"fab fa-linkedin " + classes.icon}></Icon>
+          </IconButton>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  )
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
--- src/components/layout.js
+++ src/components/layout.js
@@ -11,6 +11,7 @@ import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
 import "./layout.css"
+import { Container } from "@material-ui/core"
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -26,21 +27,13 @@ const Layout = ({ children }) => {
   return (
     <>
       <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0px 1.0875rem 1.45rem`,
-          paddingTop: 0,
-        }}
-      >
+      <Container maxWidth="lg">
         <main>{children}</main>
         <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
+          © James Woods, 2019. Built using{` `}
+          <a href="https://www.gatsbyjs.org">Gatsby</a> with ❤️ in London, UK
         </footer>
-      </div>
+      </Container>
     </>
   )
 }
