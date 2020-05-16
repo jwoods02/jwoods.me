88d401f8f9aa2e82db7004067f39cc31dcb02ad0
James Woods <jwwoods01@gmail.com>
2020-05-16 20:33:27 +0100

    feat: add content and styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,14 @@ module.exports = {
     './src/**/*.html',
   ],
   theme: {
-    extend: {},
+    fontFamily: {
+      mono: ['"Fira Code"','Menlo', 'Monaco', 'Consolas', '"Liberation Mono"', '"Courier New"', 'monospace'],
+      },
+      extend: {}
   },
-  variants: {},
+  variants: {
+    borderWidth: ['hover'],
+    backgroundColor: ['responsive', 'hover', 'focus', 'group-hover'],
+  },
   plugins: [],
 };
